d4080713f263ba5ffa09b69d763d64a540ab0f8d
AnkitaKoley81 <ankitakoley81@gmail.com>
2025-06-23 12:05:05 +0530

    second commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT;
 // })
 
 app.use(cors({
-    origin:"http://localhost:5174",
+    origin:"http://localhost:5173",
     credentials:true // allow frontend to send cookies
 }))
 app.use(express.json());
